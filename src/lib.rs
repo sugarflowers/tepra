6c9769fa493df62f3177305c44c472fa5830615f
sugarflowers <128127805+sugarflowers@users.noreply.github.com>
2025-04-10 16:02:10 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use regex::Regex;
 use std::env;
 use std::ffi::OsString;
 
-const DEFAULT_TEPRA_PATH:OsString = OsString::from(r#"c:\Program Files (x86)\KING JIM\TEPRA SPC10\SPC10.exe"#);
+//const DEFAULT_TEPRA_PATH:OsString = OsString::from(r#"c:\Program Files (x86)\KING JIM\TEPRA SPC10\SPC10.exe"#);
 
 macro_rules! cwd {
     () => {
@@ -28,6 +28,7 @@ pub struct TEPRA {
 impl TEPRA {
 
     pub fn new(tepra_path: Option<&str>) -> Self {
+        let default_tepra_path:OsString = OsString::from(r#"c:\Program Files (x86)\KING JIM\TEPRA SPC10\SPC10.exe"#);
         let tepra_path = tepra_path.unwrap_or(DEFAULT_TEPRA_PATH);
 
         let tmp = format!("{}\\tepesize.txt", std::env::var("TEMP").unwrap_or_else(|_| "c:\\".to_string()));
