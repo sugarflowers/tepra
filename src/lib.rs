f0d7c509725f91d08a533e0e1315e782f528833b
sugarflowers <128127805+sugarflowers@users.noreply.github.com>
2025-04-10 16:06:18 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@ impl TEPRA {
 
     pub fn new(tepra_path: Option<&OsString>) -> Self {
         let default_tepra_path:OsString = OsString::from(r#"c:\Program Files (x86)\KING JIM\TEPRA SPC10\SPC10.exe"#);
-        let tepra_path:OsString = tepra_path.unwrap_or(default_tepra_path);
+        let tepra_path:OsString = tepra_path.unwrap_or(&default_tepra_path);
 
         let tmp = format!("{}\\tepesize.txt", std::env::var("TEMP").unwrap_or_else(|_| "c:\\".to_string()));
         Self {
