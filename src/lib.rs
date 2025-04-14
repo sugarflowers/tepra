8ba2d2405fccad767f1aa37d069ab79bc78e7c6b
sugarflowers <128127805+sugarflowers@users.noreply.github.com>
2025-04-14 10:25:50 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -55,7 +55,7 @@ impl TEPRA {
 
     pub fn print(&self) -> Result<()> {
 
-        let param = format!(r#"{},{},{}"#, 
+        let param = format!(r#""{},{},{}""#, 
             self.tpe_path.to_string_lossy(),
             self.csv_path.to_string_lossy(), 
             self.num_print 
