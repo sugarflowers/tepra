b683d15b58ffc6b43b5830f8143304db417471e6
sugarflowers <128127805+sugarflowers@users.noreply.github.com>
2025-04-14 14:21:14 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -55,7 +55,7 @@ impl TEPRA {
     }
 
     pub fn batch(mut self, path:OsString) -> Self {
-        self.bat_path = path.into_string();
+        self.bat_path = path.into_string().unwrap();
         self
     }
     
